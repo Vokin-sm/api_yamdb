2b91fd74ee86ae0784cb1da7eb01032bb2506e7a
Ivan Safronov <ivansfr@yandex.ru>
2021-06-16 23:00:14 +0300

    fix permisson

--- api/permissions.py
+++ api/permissions.py
@@ -14,7 +14,7 @@ class IsAdmin(permissions.IsAdminUser):
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if request.user.is_authenticated:
-            return bool(request.user.is_staff or request.user.role == 'admin')
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated
+                and bool(request.user.is_staff or request.user.role == 'admin')
+                )
