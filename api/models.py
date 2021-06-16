bebb64c279768ada8efbe06bd3382cd5c0bc614b
Ivan Safronov <ivansfr@yandex.ru>
2021-06-16 20:57:26 +0300

    categories

--- api/models.py
+++ api/models.py
@@ -99,6 +99,7 @@ class Categories(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+        ordering = ('-name', )
 
     def __str__(self):
         return self.name
