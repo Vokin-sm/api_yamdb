eff0cdd7dbefe9967947369c8196d175d4368287
Ivan Safronov <ivansfr@yandex.ru>
2021-06-23 11:17:14 +0300

    added year validator

--- api/models.py
+++ api/models.py
@@ -9,6 +9,8 @@ from django.core.exceptions import ValidationError
 
 
 def validate_year(value):
+    """ Validate, that year is not grater current one """
+    
     if value > datetime.now().year:
         raise ValidationError(
             'Введенный год больше текущего'
